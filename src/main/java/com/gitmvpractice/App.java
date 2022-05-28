1749408a37cfeff09feec3661f83eaba2080d68b
mdbasheer333 <mdbasheer333@gmail.com>
2022-05-28 18:39:09 +0530

    this is my 1st commit

---
+++ src/main/java/com/gitmvpractice/App.java
@@ -0,0 +1,13 @@
+package com.gitmvpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
